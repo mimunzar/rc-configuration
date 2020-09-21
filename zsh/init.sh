5a837084b55977aba1f22b08bb3ba24e7fdc1c66
Milan Munzar <mimunzar@gmail.com>
2020-09-21 22:35:06 +0200

    More coloring for ClojureScript compiler output

--- zsh/init.sh
+++ zsh/init.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env zsh
 
 function sortConfigDirectory {
-	echo `ls -v $1/*.conf`
+    echo `ls -v $1/*.conf`
 }
 
 function loadConfiguration {
-	for conf_file in `sortConfigDirectory $1`; do
-		source $conf_file
-	done
+    for conf_file in `sortConfigDirectory $1`; do
+        source $conf_file
+    done
 }
 
 loadConfiguration $1
